9898337cf182d660b04937cb70242cf0b35bfe54
beny996 <94617004+beny996@users.noreply.github.com>
2022-10-05 22:06:53 +0200

    initial commit

---
+++ src/components/ForgotPassword.js
@@ -0,0 +1,56 @@
+import React, { useRef, useState } from "react";
+import { Card, Button, Form, Alert } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+
+const ForgotPassword = () => {
+  const emailRef = useRef();
+  const { resetPassword } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setMessage("");
+      setError("");
+      setLoading(true);
+      await resetPassword(emailRef.current.value);
+      setMessage("Check your inbox for futher instructions");
+    } catch {
+      setError("Failed to reset password");
+    }
+    setLoading(false);
+  };
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Password reset</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required></Form.Control>
+            </Form.Group>
+
+            <Button className="w-100" type="submit" disabled={loading}>
+              Reset password
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/login">Login</Link>
+          </div>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">Sign up</Link>
+      </div>
+    </>
+  );
+};
+
+export default ForgotPassword;
---
+++ src/components/Login.js
@@ -0,0 +1,64 @@
+import React, { useRef, useState } from "react";
+import { Card, Button, Form, Alert } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      navigate("/");
+    } catch {
+      setError("Failed to sign in");
+    }
+    setLoading(false);
+    login(emailRef.current.value, passwordRef.current.value);
+  };
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log in</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required></Form.Control>
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                required
+              ></Form.Control>
+            </Form.Group>
+
+            <Button className="w-100" type="submit" disabled={loading}>
+              Log in
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Forgot password?</Link>
+          </div>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">Sign up</Link>
+      </div>
+    </>
+  );
+};
+
+export default Login;
